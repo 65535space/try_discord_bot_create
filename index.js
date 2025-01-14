58de630541588cbd3e792a141c9396ed41b38dcc
65535 <93774811+65535space@users.noreply.github.com>
2025-01-15 08:19:35 +0900

    コマンドを呼び出せるようにした。

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
+// hey.jsのmodule.exportsを呼び出す
+const heyFile = require('./commands/hey.js');
+
 // discord.jsライブラリの中から必要な設定を呼び出す。
 const { Client, Events, GatewayIntentBits } = require('discord.js');
 
 // 設定ファイルからトークン情報を呼び出し、変数に保存します
 const {token} = require('./config.json');
-
+// クライアントインスタンスと呼ばれるオブジェクトを作成する
 const client = new Client({ intents: [GatewayIntentBits.Guilds]});
 
 
-// クライアントオブジェクトが準備OKとなったとき一度だけ実行されます
+// クライアントオブジェクトが準備OKとなったとき一度だけ実行される
 client.once(Events.ClientReady, c => {
     console.log(`準備OKです! ${c.user.tag} がログインします`);
 });
 
 
-//スラッシュコマンドに応答するには、interactionCreateeのイベントリスナーを使う必要があります
+//スラッシュコマンドに応答するには、interactionCreateeのイベントリスナーを使う必要がある
 client.on(Events.InteractionCreate, async interaction => {
 
     // スラッシュ以外のコマンドの場合は対象外なので早期リターンさせて終了する
-    // コマンドにスラッシュが使われているかどうかはisChatInputCommand()で判断しています
+    // コマンドにスラッシュが使われているかどうかはisChatInputCommand()で判断している
     if (!interaction.isChatInputCommand()) return;
 
     // heyコマンドに対する処理
@@ -37,5 +40,5 @@ client.on(Events.InteractionCreate, async interaction => {
     }
 });
 
-// ログインします
+// ログインする
 client.login(token);
